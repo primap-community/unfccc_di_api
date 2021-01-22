97e94ecd018e84727ad288c566f1d8786c89adc3
Mika Pflüger <mika.pflueger@pik-potsdam.de>
2021-01-22 15:36:13 +0100

    Bump version: 0.1.0 → 1.0.0

--- setup.py
+++ setup.py
@@ -50,6 +50,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/pik-primap/unfccc_di_api",
-    version="0.1.0",
+    version="1.0.0",
     zip_safe=False,
 )
