7245a34e3ff8977e7ffa02aa598027d39d7648db
Mika Pflüger <mika.pflueger@climate-resource.com>
2023-11-13 15:21:11 +0100

    test: limit UNFCCC api reader testing to github actions

--- unfccc_di_api/tests/test_unfccc_di_api.py
+++ unfccc_di_api/tests/test_unfccc_di_api.py
@@ -48,7 +48,9 @@ def test_query(reader):
     assert len(res) > 0
 
 
-#@pytest.mark.skip(reason="UNFCCC reader not available")
+@pytest.mark.skipif(
+    "GITHUB_ACTIONS" not in os.environ, reason="UNFCCC reader not available"
+)
 def test_non_annex_one(api_reader):
     ans = api_reader.non_annex_one_reader.query(party_codes=["MMR"])
 
@@ -59,7 +61,9 @@ def test_non_annex_one(api_reader):
     assert len(ans) > 1
 
 
-#@pytest.mark.skip(reason="UNFCCC reader not available")
+@pytest.mark.skipif(
+    "GITHUB_ACTIONS" not in os.environ, reason="UNFCCC reader not available"
+)
 def test_annex_one(api_reader: UNFCCCApiReader):
     ans = api_reader.annex_one_reader.query(party_codes=["DEU"], gases=["N₂O"])
     assert len(ans) > 1
@@ -74,13 +78,17 @@ def test_unified(reader):
         reader.query(party_code="ASDF")
 
 
-#@pytest.mark.skip(reason="UNFCCC reader not available")
+@pytest.mark.skipif(
+    "GITHUB_ACTIONS" not in os.environ, reason="UNFCCC reader not available"
+)
 def test_unified_gases(api_reader: UNFCCCApiReader):
     ans = api_reader.query(party_code="DEU", gases=["N₂O"])
     assert len(ans) > 1
 
 
-#@pytest.mark.skip(reason="UNFCCC reader not available")
+@pytest.mark.skipif(
+    "GITHUB_ACTIONS" not in os.environ, reason="UNFCCC reader not available"
+)
 @pytest.mark.parametrize("normalize", [True, False])
 def test_unified_as_ascii(api_reader: UNFCCCApiReader, normalize: bool):
     # assert that using standardized string ('N2O' instead of "N₂O") works
@@ -91,7 +99,9 @@ def test_unified_as_ascii(api_reader: UNFCCCApiReader, normalize: bool):
     assert ans.gas.unique()[0] == ("N2O" if normalize else "N₂O")
 
 
-#@pytest.mark.skip(reason="UNFCCC reader not available")
+@pytest.mark.skipif(
+    "GITHUB_ACTIONS" not in os.environ, reason="UNFCCC reader not available"
+)
 @pytest.mark.parametrize("category_id", [9559, 9608])
 def test_category_filter(api_reader: UNFCCCApiReader, category_id):
     # this failed due to duplicate variableIds
@@ -102,7 +112,9 @@ def test_category_filter(api_reader: UNFCCCApiReader, category_id):
     assert len(df["category"].unique()) == 1
 
 
-#@pytest.mark.skip(reason="UNFCCC reader not available")
+@pytest.mark.skipif(
+    "GITHUB_ACTIONS" not in os.environ, reason="UNFCCC reader not available"
+)
 def test_no_data(api_reader: UNFCCCApiReader):
     with pytest.raises(unfccc_di_api.NoDataError):
         api_reader.annex_one_reader.query(party_codes=["FIN"], category_ids=[14817])
